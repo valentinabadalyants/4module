fb86095c2408da380206e56e435e86e946b67a9c
valentinabadalyants <113968650+valentinabadalyants@users.noreply.github.com>
2023-06-23 13:47:40 +0300

    Initial commit

---
+++ javascripts/alljs.js
@@ -0,0 +1,108 @@
+$(document).ready(function(){
+  $(".r1").hover(function(){
+    $('.r1').toggleClass("hovered");
+    $('.kl1').toggleClass("hovered1");
+ });
+});
+$(document).ready(function(){
+  $(".r2").hover(function(){
+    $('.r1').toggleClass("hovered");
+    $('.kl1').toggleClass("hovered1");
+    $('.r2').toggleClass("hovered3");
+    $('.kl2').toggleClass("hovered4");
+ });
+});
+$(document).ready(function(){
+  $(".r3").hover(function(){
+    $('.r1').toggleClass("hovered");
+    $('.kl1').toggleClass("hovered1");
+    $('.r2').toggleClass("hovered3");
+    $('.kl2').toggleClass("hovered4");
+    $('.r3').toggleClass("hovered5");
+    $('.kl3').toggleClass("hovered6");
+ });
+});
+// $(document).ready(function(){
+//   $(".logo").click(function(){
+//     window.location.href= "mainpaige.html";
+//     return false;
+//   });
+// });
+function myFunction() {
+  window.location.href="mainpaige.html";
+}
+
+// для магазина появление картинки
+$(document).ready(function(){
+  $(".b10").hover(function(){
+    $('.sh10').toggleClass("in");
+    $('.in10').toggleClass("op");
+ });
+});
+$(document).ready(function(){
+  $(".b11").hover(function(){
+    $('.sh11').toggleClass("in");
+    $('.in11').toggleClass("op");
+ });
+});
+$(document).ready(function(){
+  $(".b12").hover(function(){
+    $('.sh12').toggleClass("in");
+    $('.in12').toggleClass("op");
+ });
+});
+$(document).ready(function(){
+  $(".b13").hover(function(){
+    $('.sh13').toggleClass("in");
+    $('.in13').toggleClass("op");
+ });
+});
+$(document).ready(function(){
+  $(".b14").hover(function(){
+    $('.sh14').toggleClass("in");
+    $('.in14').toggleClass("op");
+ });
+});
+$(document).ready(function(){
+  $(".b15").hover(function(){
+    $('.sh15').toggleClass("in");
+    $('.in15').toggleClass("op");
+ });
+});
+$(document).ready(function(){
+  $(".b16").hover(function(){
+    $('.sh16').toggleClass("in");
+    $('.in16').toggleClass("op");
+ });
+});
+
+
+function myFunction2() {
+  window.location.href="t-shirt.html";
+}
+function myFunction3() {
+  window.location.href="sweater.html";
+}
+function myFunction4() {
+  window.location.href="case.html";
+}
+function myFunction5() {
+  window.location.href="bags.html";
+}
+function myFunction6() {
+  window.location.href="scarf.html";
+}
+function myFunction7() {
+  window.location.href="bag.html";
+}
+function myFunction8() {
+  window.location.href="brelock.html";
+}
+
+
+
+function myFunction9() {
+  window.location.href="basket.html";
+}function myFunction10() {
+  window.location.href="shop.html";
+}
